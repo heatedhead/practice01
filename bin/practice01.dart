e37e5252381fc4eb903d74cf0ead3a54ab324d95
Anil Raj <Aniljmjr@gmail.com>
2022-07-13 10:18:52 +0530

    My new project

--- bin/practice01.dart
+++ bin/practice01.dart
@@ -1,3 +1,35 @@
-void main(){
+import 'dart:io';
+
+void main(){//var name="Anil";
+  //var lname ="Raj";
+  //var age = 20;
+ // stdout.write("Enter your gender\n");
+ // var gender;
+
+  //gender=stdin.readLineSync();
+  //print("Your gender is :${gender="Female"}");
+ // print(gender);
+
+ // var email="anil@gmail.com";
+  //final String course= "Android Flutter";
+ // stdout.write("my name is ${name+" "+lname}\n"
+//"I am $age years old\n      my email id is $email"
+//"I am doing $course at lumi");
+
+
+//String name= "Anil";
+//String Lname ="Raj";
+//String Sname = "M";
+//stdout.write("My name is ${name+" "+Lname+" "+Sname}");
+
+//String name = "Anil";
+//print("${name+="'s Home"}");
+//stdout.write("$name");
+
+//String gender ="Male";
+//print("${gender+="Test"}");
+const String hobby = "riding";
+print("My fav hobby is $hobby");
+print("My fav hobby is "+hobby);
 
 }
